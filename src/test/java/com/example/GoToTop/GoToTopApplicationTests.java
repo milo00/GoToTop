1fdcea3c4019e7cb4aaa7944a1b765a8f06e3416
Milena <milenamazur00@gmail.com>
2022-01-13 17:39:27 +0100

    init commit

---
+++ src/test/java/com/example/GoToTop/GoToTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoToTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoToTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
